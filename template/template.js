2006a4e1c895702460618f8b218cb063a87ba1ef
Au Rob <robau6@gmail.com>
2025-12-13 03:02:02 -0500

    adding books page

--- template/template.js
+++ template/template.js
@@ -167,17 +167,22 @@ window.addEventListener('load', ()=>{
     if (!SITEMAP) {
         SITEMAP = {
             ".": {},
+            "webring": {},
+            "photo": {},
+            "music": {},
+            "books": {},
+            "links": {},
+            "genart": {},
+            "doodles": {},
             "advent": {
                 "day1": {},
                 "day2": {},
                 "day3": {},
-                "day4": {}
+                "day4": {},
+                "day5": {},
+                "day6": {},
+                "day7": {}
             },
-            "webring": {},
-            "photo": {},
-            "links": {},
-            "genart": {},
-            "doodles": {}
         };
         sessionStorage.setItem('sitemap', JSON.stringify(SITEMAP));
     }
